1a8f26f1dec49e664b2687cfe04825437538d541
41521339880 <41521339880@info.local>
2017-11-29 19:12:25 -0200

    Migração para React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
+import FlexiblePlatform from "./components/FlexiblePlatform/FlexiblePlatform";
 
 export default () => (
   <div className="App">
