7cedb6a9cae466c4465d2af53a472d1474e0073f
Lucia de Menezes Farias <95110348049@jpmorgan.sp>
2017-11-29 19:16:37 -0200

    primeiras tentativas Lucia

---
+++ src/components/byob-form/byob-form.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "byob-form.css"
+
+export default (props) => (
+    <header className="byob??">
+        <div>
+        <form className="byob-form">
+        <input className="byob-form__input" type="text" name="email" value="Add your email address">
+        <a href="" className="byob-form__button">invite me</a>
+        </form>
+        </div>
+    </header>
+);
