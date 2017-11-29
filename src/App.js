12ec0bff71f613a017850cdafecfe2defae2801b
MARINA ALVES QUEIROZ MALUTA <01547014679@jpmorgan.sp>
2017-11-29 19:40:26 -0200

    Acrescentando componente no app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
 
 export default () => (
   <div className="App">
     <Hero />
+    <Footer />
   </div>
 );
+
