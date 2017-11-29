080246758d92fd44bc2cbf4b28341d38146c33e3
MARINA ALVES QUEIROZ MALUTA <01547014679@jpmorgan.sp>
2017-11-29 20:16:16 -0200

    Indentação ok!

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,6 @@ import "./Footer.css"
 
 export default (props) => (
 <footer className="rodape">
-
-        <p>2014 Shooting Brake Design</p>
-
-    </footer>
+    <p>2014 Shooting Brake Design</p>
+</footer>
 ); 
