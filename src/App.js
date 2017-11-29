86300d90e3a1e3816eeb4c27b4574a805f58264f
rplandrade <rplandrade@gmail.com>
2017-11-29 19:33:42 -0200

    add components features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
+import Features from "./components/Features/Features"
 
 export default () => (
   <div className="App">
     <Hero />
+    <Features />
   </div>
 );
