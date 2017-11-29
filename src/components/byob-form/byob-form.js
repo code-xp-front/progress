97f65365b0035e0bbb8201a9016f36feae3fc752
Lucia de Menezes Farias <95110348049@jpmorgan.sp>
2017-11-29 20:21:48 -0200

    FUNCIONANDO CARAAAAAAAAALHO!!!!

--- src/components/byob-form/byob-form.js
+++ src/components/byob-form/byob-form.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "byob-form.css"
+import "./byob-form.css"
 
 export default (props) => (
         <div>
         <form className="byob-form">
-        <input className="byob-form__input" type="text" name="email" value="Add your email address">
-        <a href="" className="byob-form__button">invite me</a>
+        <input className="byob-form__input" type="text" name="email" value={props.email}/>
+        <a href="" className="byob-form__button">{props.description}</a>
         </form>
         </div>
 );
+
+// <ByobForm description="Invite Me" email="Add your mail here"/>
