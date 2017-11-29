c841226753d19075b7e70dfe80e36b1dcc9054c2
DANIELLE NAOMI NAKATSU <38386161892@jpmorgan.sp>
2017-11-29 19:15:55 -0200

    bring text change

--- src/components/Bring/Bring.js
+++ src/components/Bring/Bring.js
@@ -18,34 +18,40 @@ var bring = {
 }
 
 export default (props) => (
-    <section class="bring">
-        <h1 class="bring-title">Bring Your Own Business</h1>
-        <div class="services">
-            <div class="services-icon services-icon--left">
-                <img class="services-icon__demo services-icon__demo--1" src="img/icon1.png" alt="icon detail">
-                <img class="services-icon__demo services-icon__demo--2" src="img/icon2.png" alt="icon flower">
-                <img class="services-icon__demo services-icon__demo--4" src="img/icon3.png" alt="icon grave">
-                <img class="services-icon__demo services-icon__demo--3" src="img/icon4.png" alt="icon pc">
+    <section className="bring">
+        <h1 className="bring-title">
+            {props.title}
+        </h1>
+        <div className="services">
+            <div className="services-icon services-icon--left">
+                <img className="services-icon__demo services-icon__demo--1" src="img/icon1.png" alt="icon detail">
+                <img className="services-icon__demo services-icon__demo--2" src="img/icon2.png" alt="icon flower">
+                <img className="services-icon__demo services-icon__demo--4" src="img/icon3.png" alt="icon grave">
+                <img className="services-icon__demo services-icon__demo--3" src="img/icon4.png" alt="icon pc">
             </div>
-            <div class="services-phone">
-                <img class="services-phone__demo" src="img/phone.png" alt="phone">
+            <div className="services-phone">
+                <img className="services-phone__demo" src="img/phone.png" alt="phone">
             </div>
-            <div class="services-icon services-icon--right">
-                <img class="services-icon__demo services-icon__demo--2" src="img/icon5.png" alt="icon bug">
-                <img class="services-icon__demo services-icon__demo--1" src="img/icon6.png" alt="icon tool">
-                <img class="services-icon__demo services-icon__demo--3" src="img/icon7.png" alt="icon mech">
-                <img class="services-icon__demo services-icon__demo--4" src="img/icon8.png" alt="icon garden">
+            <div className="services-icon services-icon--right">
+                <img className="services-icon__demo services-icon__demo--2" src="img/icon5.png" alt="icon bug">
+                <img className="services-icon__demo services-icon__demo--1" src="img/icon6.png" alt="icon tool">
+                <img className="services-icon__demo services-icon__demo--3" src="img/icon7.png" alt="icon mech">
+                <img className="services-icon__demo services-icon__demo--4" src="img/icon8.png" alt="icon garden">
             </div>
         </div>
 
-        <div class="bring-text">
-            <h2 class="bring-subtitle">Flexible Platform</h2>
-            <p class="bring-parag">We are an industry agnostic platform. If you can track it, you can Progress it! Progress makes it easy for service providers to connect directly with their customers.</p>
+        <div className="bring-text">
+            <h2 className="bring-subtitle">
+                {props.subtitle}
+            </h2>
+            <p className="bring-parag">
+                {props.description}
+            </p>
         </div>
 
-        <form class="invite">
-            <input class="invite__input" type="email" placeholder="Add your email address">
-            <button class="invite__button">Invite me</button>
+        <form className="invite">
+            <input className="invite__input" type="email" placeholder="Add your email address">
+            <button className="invite__button">Invite me</button>
         </form>
     </section>
 );
