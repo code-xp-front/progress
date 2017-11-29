0798907fa8a15853a73616cd41fde4be42099764
DEISE SAYURI AHAGON KOREEDA <36918838857@jpmorgan.sp>
2017-11-29 19:38:39 -0200

    finished footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
 import FollowAlong from "./components/Follow-along/Follow-along";
+import Footer from "./components/Footer/Footer";
 
 export default () => (
   <div className="App">
     <Hero />
     <FollowAlong description="Follow along on Twitter for all the lastest news!" />
+    <Footer text="2014 Shooting Brake Design"></Footer>
   </div>
 );
