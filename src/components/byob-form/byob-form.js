7de0a71b6b3e5adf6f5c82d2ce3709a2b4bab1da
Lucia de Menezes Farias <95110348049@jpmorgan.sp>
2017-11-29 19:40:09 -0200

    tirei o header que estava sobrando

--- src/components/byob-form/byob-form.js
+++ src/components/byob-form/byob-form.js
@@ -2,12 +2,10 @@ import React from "react";
 import "byob-form.css"
 
 export default (props) => (
-    <header className="byob??">
         <div>
         <form className="byob-form">
         <input className="byob-form__input" type="text" name="email" value="Add your email address">
         <a href="" className="byob-form__button">invite me</a>
         </form>
         </div>
-    </header>
 );
