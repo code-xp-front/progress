9dcc795b8d40196fe95cee0a2e1ca75cc2642ca7
JULIANA DE CARVALHO MAZZO <35677617814@jpmorgan.sp>
2017-11-29 20:18:23 -0200

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Footer.css"
+
+
+export default (props) => (
+<footer class="footer">
+        <p class="footer__text">© 2017 El Carrasco Design</p>
+    </footer>
+);
