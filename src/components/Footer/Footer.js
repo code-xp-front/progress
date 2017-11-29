5740802a6703796921fe90dd2cdc98175702d7d2
OrleansKlaus <orleansklaus@gmail.com>
2017-11-29 20:20:07 -0200

    Atualizacao Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css"
 
 export default (props) => (
-    <footer class="rodape">
+    <footer className="rodape">
         <p>{props.title}</p>
     </footer>
 );
