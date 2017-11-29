c18571f087f86a5d79db811baea255787293addd
DANIELLE NAOMI NAKATSU <38386161892@jpmorgan.sp>
2017-11-29 19:37:42 -0200

    npm install

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
+import Bring from "./components/Bring/Bring";
 
 export default () => (
   <div className="App">
     <Hero />
+    <Bring />
   </div>
 );
--- src/components/Bring/Bring.js
+++ src/components/Bring/Bring.js
@@ -14,42 +14,42 @@ var bring = {
     title: "Bring Your Own Business",
     subtitle:"Flexible Platform",
     description: "We are an industry agnostic platform. If you can track it, you can Progress it! Progress makes it easy for service providers to connect directly with their customers."
-}
+};
 
 export default (props) => (
     <section className="bring">
         <h1 className="bring-title">
-            {props.bring.title}
+            {bring.title}
         </h1>
         <div className="services">
             <div className="services-icon services-icon--left">
-                <img className="services-icon__demo services-icon__demo--1" src={imgService1} alt="icon detail">
-                <img className="services-icon__demo services-icon__demo--2" src={imgService2} alt="icon flower">
-                <img className="services-icon__demo services-icon__demo--4" src={imgService3} alt="icon grave">
-                <img className="services-icon__demo services-icon__demo--3" src={imgService4} alt="icon pc">
+                <img className="services-icon__demo services-icon__demo--1" src={imgService1} alt="icon detail"/>
+                <img className="services-icon__demo services-icon__demo--2" src={imgService2} alt="icon flower"/>
+                <img className="services-icon__demo services-icon__demo--4" src={imgService3} alt="icon grave"/>
+                <img className="services-icon__demo services-icon__demo--3" src={imgService4} alt="icon pc"/>
             </div>
             <div className="services-phone">
-                <img className="services-phone__demo" src={imgPhone} alt="phone">
+                <img className="services-phone__demo" src={imgPhone} alt="phone"/>
             </div>
             <div className="services-icon services-icon--right">
-                <img className="services-icon__demo services-icon__demo--2" src={imgService5} alt="icon bug">
-                <img className="services-icon__demo services-icon__demo--1" src={imgService6} alt="icon tool">
-                <img className="services-icon__demo services-icon__demo--3" src={imgService7} alt="icon mech">
-                <img className="services-icon__demo services-icon__demo--4" src={imgService8} alt="icon garden">
+                <img className="services-icon__demo services-icon__demo--2" src={imgService5} alt="icon bug"/>
+                <img className="services-icon__demo services-icon__demo--1" src={imgService6} alt="icon tool"/>
+                <img className="services-icon__demo services-icon__demo--3" src={imgService7} alt="icon mech"/>
+                <img className="services-icon__demo services-icon__demo--4" src={imgService8} alt="icon garden"/>
             </div>
         </div>
 
         <div className="bring-text">
             <h2 className="bring-subtitle">
-                {props.bring.subtitle}
+                {bring.subtitle}
             </h2>
             <p className="bring-parag">
-                {props.bring.description}
+                {bring.description}
             </p>
         </div>
 
         <form className="invite">
-            <input className="invite__input" type="email" placeholder="Add your email address">
+            <input className="invite__input" type="email" placeholder="Add your email address"/>
             <button className="invite__button">Invite me</button>
         </form>
     </section>
