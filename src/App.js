f89f8a5f26a10b528f3a5bcb4b6d45ac923c3af5
YVES LEONARDO CARRASCO PEREIRA <39366274870@jpmorgan.sp>
2017-11-29 19:09:18 -0200

    Section Services transformed for react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
 
 export default () => (
   <div className="App">
     <Hero />
+    <Services />
   </div>
 );
