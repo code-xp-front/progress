753aabe0dea15803479570724971aa75f97ac5d0
YVES LEONARDO CARRASCO PEREIRA <39366274870@jpmorgan.sp>
2017-11-30 19:01:05 -0200

    X

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,24 +4,25 @@ import "./Order.css";
 var empresa ()
 
 export default (props) => (
-    <section class="order">
+<section class="order">
     <div class="order___entreprise">
-        <h3></h3>
-        <span></span>
-        <span></span>
-        <h3></h3>
+        <h3>euro</h3>
+        <span>water</span>
+        <span>prepared</span>
+        <h3>jake</h3>
     </div>
     <div class="order__details">
-        <h3></h3>
-        <h3></h3>
-        <span></span>
-        <span></span>
+        <h3>Estimate</h3>
+        <h3>Progress</h3>
+        <span>Vehicule</span>
+        <span>2010 Mini</span>
     </div>
     <div class="order__description">
-
+    Description Qty Price Amount
     </div>
     <div class="order__submit">
-        
+        <button> approve </button>
+        <button> decline </button>
     </div>
 </section>
 );
