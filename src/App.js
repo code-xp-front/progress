b49bd58d9f4c85ea534618855ebc445101ad1a49
YVES LEONARDO CARRASCO PEREIRA <39366274870@jpmorgan.sp>
2017-11-30 19:35:51 -0200

    variables from table

--- src/App.js
+++ src/App.js
@@ -3,12 +3,45 @@ import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
 import Services from "./components/Services/Services";
 import FlexiblePlatform from "./components/FlexiblePlatform/FlexiblePlatform";
+import Order from "./components/Order/Order";
 
+var service1 = {
+    description: "Tensioner",
+    qty: 1,
+    price: 45.00,
+    amount: 45.00
+};
+
+var service2 = {
+    description: "Oil Filter",
+    qty: 1,
+    price: 103.00,
+    amount: 103.00
+};
+
+var service3 = {
+    description: "Bilstein 5100 Shocks",
+    qty: 4,
+    price: 200.00,
+    amount: 800.00
+};
+
+var service4 = {
+    description: "Labor (per hour)",
+    qty: 14,
+    price: 70.00,
+    amount: 980.00
+};
+var subtotal = (service1.amount + service2.amount + service3.amount + service4.amount);
+var tax = (115.68);
+
+var total = (subtotal + tax);
 
 export default () => (
   <div className="App">
     <Hero />
     <Features />
+    <Order />
     <Services />
   </div>
 );
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,26 +1,50 @@
 import React from "react";
 import "./Order.css";
 
-var empresa ()
-
 export default (props) => (
-<section class="order">
-    <div class="order___entreprise">
+<section className="order">
+    <div className="order___entreprise">
         <h3>euro</h3>
         <span>water</span>
         <span>prepared</span>
         <h3>jake</h3>
     </div>
-    <div class="order__details">
+    <div className="order__details">
         <h3>Estimate</h3>
         <h3>Progress</h3>
         <span>Vehicule</span>
         <span>2010 Mini</span>
     </div>
-    <div class="order__description">
-    Description Qty Price Amount
+    <div className="order__description">
+        <table>
+           <tr>
+                <th>Description</th>
+                <th>Qty</th> 
+                <th>Price</th>
+                <th>Amount</th>
+           </tr>
+           <tr>
+                <td>{props.service1.qty}</td>
+                <td>{props.service2.qty}</td> 
+                <td>{props.service3.qty}</td>
+                <td>{props.service4.qty}</td>
+           </tr>
+           <tr>
+                <td>{props.service1.price}</td>
+                <td>{props.service2.price}</td> 
+                <td>{props.service3.price}</td>
+                <td>{props.service4.price}</td>
+           </tr>
+           <tr>
+                <td>{props.service1.amount}</td>
+                <td>{props.service2.amount}</td> 
+                <td>{props.service3.amount}</td>
+                <td>{props.service4.amount}</td>
+           </tr>
+
+        </table>
     </div>
-    <div class="order__submit">
+    <div className="order__submit">
         <button> approve </button>
         <button> decline </button>
     </div>
