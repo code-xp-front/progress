7f336150afdc3797435cfce699c4eddcfe397a96
41521339880 <41521339880@info.local>
2017-11-30 19:04:56 -0200

    Criação de variáveis

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Order.css";
 
-var empresa ()
+var nome_empresa = 'EuroTech Motors, LLC';
+var endereco = '555. Water Town Court, Holland, MI 49242'
+var pedido = '#18801934';
+var progress_code = '3110556';
+var nome_vendedor = 'Jake Montgomery';
+var ano_veiculo = '2010';
+var modelo_veiculo = 'Mini Cooper S';
+var milhas = '31.209';
+var VIN = '000193HHAAO1I345';
+
+
+
 
 export default (props) => (
     <section class="order">
